08d4ef2ac5fe4350065a5d27daf23876e6442f99
Alexander Gluschenko <qugluschenko@gmail.com>
2026-03-18 00:00:52 +0400

    Update OnnxModel.cs

--- src/Onnxify/OnnxModel.cs
+++ src/Onnxify/OnnxModel.cs
@@ -3,7 +3,6 @@ using Onnx;
 using System.Collections.ObjectModel;
 using System.Numerics;
 using System.Runtime.InteropServices;
-using System.Linq;
 
 namespace Onnxify;
 
@@ -897,6 +896,7 @@ public class OnnxTensor<T> : OnnxTensor
     public override string Name => _tensor.Name;
     public override TensorProto.Types.DataType DataType => (TensorProto.Types.DataType)_tensor.DataType;
     public TensorProto.Types.DataLocation DataLocation { set; get; }
+    public IEnumerable<long> Shape { get; set; }
     public IEnumerable<T> Value { get; set; }
 
     private readonly TensorProto _tensor;
@@ -906,6 +906,7 @@ public class OnnxTensor<T> : OnnxTensor
         _tensor = tensor;
 
         DataLocation = tensor.DataLocation;
+        Shape = tensor.Dims.ToList();
         Value = OnnxHelper.GetValue<T>(tensor);
     }
 
@@ -915,6 +916,15 @@ public class OnnxTensor<T> : OnnxTensor
         newTensor.Name = Name;
         newTensor.DataType = (int)DataType;
         newTensor.DataLocation = DataLocation;
+
+        newTensor.Dims.Clear();
+        foreach (var size in Shape)
+        {
+            newTensor.Dims.Add(size);
+        }
+
+        var data = Value.ToArray() ?? [];
+        newTensor.SetValue(data, _tensor.Dims.ToArray());
 
         return newTensor;
     }
